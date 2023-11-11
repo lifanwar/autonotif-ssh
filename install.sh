564b49049d0b7ee7b7cf8868b3df3f0863c54527
lifanwar <52408309+lifanwar@users.noreply.github.com>
2023-11-11 16:25:04 +0200

    Update install.sh
    Add source code

--- install.sh
+++ install.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+# install geoiplookup
+if ! command -v geoiplookup &> /dev/null; then
+    echo "Installing geoiplookup..."
+    sudo apt install geoip-bin
+fi
+
+# execution telegram.sh file
+chmod +x telegramnotif.sh
+
+# waiting
+sleep 1
+
+# move telegram to executable
+sudo mv telegram.sh /usr/bin/telegramnotif
+
+# make telegram executable as root 
+sudo chown root:root /usr/bin/telegramnotif
+
+# Secure it with permissions to prevent privilege escalation
+sudo chmod 755 /usr/bin/telegramnotif
+
+# move notify.sh to file auto send
+sudo mv notify.sh /etc/profile.d/notify.sh
+
+# check if telegram bot can send chat
+telegramnotif "Succsess Connected"
+echo "Succsess connected"
